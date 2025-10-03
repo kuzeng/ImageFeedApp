f50309fc8e29d6d9c3f1429ea74b452b033f6951
kuzeng <kuiduanz@gmail.com>
2025-10-02 23:53:14 -0700

    Move Feedimage mapping to the FeedItemsMapper

--- EssentialFeed/EssentialFeed/Image Comments API/ImageCommentsMapper.swift
+++ EssentialFeed/EssentialFeed/Image Comments API/ImageCommentsMapper.swift
@@ -42,9 +42,3 @@ final class ImageCommentsMapper {
         (200...299).contains(response.statusCode)
     }
 }
-
-private extension Array where Element == RemoteFeedItem {
-    func toModels() -> [FeedImage] {
-        map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.image) }
-    }
-}
