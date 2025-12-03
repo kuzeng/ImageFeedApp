9a064a8ffb200fd70cd7e36369cdc9bc34a5e44e
kuzeng <kuiduanz@gmail.com>
2025-12-02 20:43:57 -0800

    Extract logic into helper methods

--- EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
+++ EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
@@ -33,7 +33,7 @@ class FeedAcceptanceTests: XCTestCase {
         XCTAssertEqual(feed.renderedFeedImageData(at: 0), makeImageData0())
         XCTAssertEqual(feed.renderedFeedImageData(at: 1), makeImageData1())
         XCTAssertEqual(feed.renderedFeedImageData(at: 2), makeImageData2())
-        XCTAssertTrue(feed.canLoadMoreFeed)
+        XCTAssertFalse(feed.canLoadMoreFeed)
     }
     
     func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
