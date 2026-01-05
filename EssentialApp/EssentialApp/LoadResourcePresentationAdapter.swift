8cec5c12d9f6aa3733aaabc133e6a8ce40517374
kuzeng <kuiduanz@gmail.com>
2026-01-04 19:44:07 -0800

    Replace Combine's `LoadResourcePresentationAdapter` with async solution

--- EssentialApp/EssentialApp/LoadResourcePresentationAdapter.swift
+++ EssentialApp/EssentialApp/LoadResourcePresentationAdapter.swift
@@ -5,12 +5,11 @@
 //  Created by KDZ on 7/20/25.
 //
 
-import Combine
 import EssentialFeed
 import EssentialFeediOS
 
 @MainActor
-final class AsyncLoadResourcePresentationAdapter<Resource, View: ResourceView> {
+final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
     private let loader: () async throws -> Resource
     private var cancellable: Task<Void, Never>?
     private var isLoading = false
@@ -49,58 +48,6 @@ final class AsyncLoadResourcePresentationAdapter<Resource, View: ResourceView> {
     }
 }
 
-extension AsyncLoadResourcePresentationAdapter: FeedImageCellControllerDelegate {
-    func didRequestImage() {
-        loadResource()
-    }
-    
-    func didCancelImageRequest() {
-        cancellable?.cancel()
-        cancellable = nil
-        isLoading = false
-    }
-}
-
-
-@MainActor
-final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
-    private let loader: () -> AnyPublisher<Resource, Error>
-    private var cancellable: Cancellable?
-    private var isLoading = false
-    
-    var presenter: LoadResourcePresenter<Resource, View>?
-    
-    init(loader: @escaping () -> AnyPublisher<Resource, Error>) {
-        self.loader = loader
-    }
-    
-    func loadResource() {
-        guard !isLoading else { return }
-        
-        presenter?.didStartLoading()
-        isLoading = true
-        
-        cancellable = loader()
-            .dispatchOnMainThread()
-            .handleEvents(receiveCancel: { [weak self] in
-                self?.isLoading = false
-            })
-            .sink(
-                receiveCompletion: { [weak self] completion in
-                    switch completion {
-                    case .finished: break
-                        
-                    case let .failure(error):
-                        self?.presenter?.didFinishLoading(with: error)
-                    }
-                    
-                    self?.isLoading = false
-                }, receiveValue: { [weak self] resource in
-                    self?.presenter?.didFinishLoading(with: resource)
-                })
-    }
-}
-
 extension LoadResourcePresentationAdapter: FeedImageCellControllerDelegate {
     func didRequestImage() {
         loadResource()
@@ -109,5 +56,6 @@ extension LoadResourcePresentationAdapter: FeedImageCellControllerDelegate {
     func didCancelImageRequest() {
         cancellable?.cancel()
         cancellable = nil
+        isLoading = false
     }
 }
